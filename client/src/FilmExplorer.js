e550b486910aba936004a92654b3ff95563ca69e
Michael Linderman <mlinderman@middlebury.edu>
2018-03-10 17:36:08 -0500

    Update setRating method in FilmExplorer to match practical guide

--- client/src/FilmExplorer.js
+++ client/src/FilmExplorer.js
@@ -32,42 +32,34 @@ class FilmExplorer extends Component {
   }
 
   setRating(filmid, rating) {
-    let film = this.state.movies.find(movie => filmid === movie.id);
-    film = Object.assign({}, film, { rating });
+    const oldMovie = this.state.movies.find(movie => filmid === movie.id);
+    const newMovie = Object.assign({}, oldMovie, { rating });
 
-    const filmStr = JSON.stringify(film);
-    const request = new Request(
-      `/api/movies/${filmid}`,
-      {
-        method: 'PUT',
-        body: filmStr,
-        headers: new Headers({ 'Content-type': 'application/json' }),
-      },
-    );
-
-    fetch(request)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(response.status_text);
+    fetch(`/api/movies/${filmid}`, {
+      method: 'PUT',
+      body: JSON.stringify(newMovie),
+      headers: new Headers({ 'Content-type': 'application/json' }),
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status_text);
+      }
+      return response.json();
+    }).then((updatedMovie) => {
+      // It would tempting to save the index where we first found the film
+      // but it is possible that state.movies has changed in the meantime
+      const updatedMovies = this.state.movies.map((movie) => {
+        if (movie.id === updatedMovie.id) {
+          return updatedMovie;
         }
-        return response.json();
-      })
-      .then((newFilm) => {
-        // It would tempting to save the index where we first found the film
-        // but it is possible that state.movies has changed in the meantime
-        const alteredFilms = this.state.movies.map((movie) => {
-          if (movie.id === newFilm.id) {
-            return newFilm;
-          }
-          return movie;
-        });
-        this.setState({ movies: alteredFilms });
-      }).catch(err => console.log(err)); // eslint-disable-line no-console
+        return movie;
+      });
+      this.setState({ movies: updatedMovies });
+    }).catch(err => console.log(err)); // eslint-disable-line no-console
   }
 
   render() {
     let movieContents = (<h2>Loading...</h2>);
-    if (this.state.movies) {
+    if (this.state.movies.length > 0) {
       movieContents = (<MovieTableContainer
         searchTerm={this.state.searchTerm}
         movies={this.state.movies}
