26a54b1d21982c3f5b247af187f7baa13bab65d5
Chris <chris.fetterolf@gmail.com>
2018-03-19 17:43:50 -0400

    test

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,30 +1,41 @@
 import React from 'react';
 //import PropTypes from 'prop-types';
 
-function Navbar(props) {
-  const Title = (
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">
-        <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" />
-        Job Search
-      </a>
-    </div>
-  );
+class Navbar extends React.Component   {
 
-  const Buttons = (
-    <button class="btn btn-outline-info my-2 my-sm-0">
-      Log In
-    </button>
-  );
+  constructor(props) {
+    super(props);
 
-  return (
+    this.state = {
+      // TODO - set state
+    };
+  }
+
+  render() {
+    const Title = (
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">
+          <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" />
+          Job Search
+        </a>
+      </div>
+    );
+
+    const Buttons = (
+      <button class="btn btn-outline-info my-2 my-sm-0">
+        Log In
+      </button>
+    );
+
+    return (
       <nav class="navbar fixed-top navbar-dark bg-dark">
         <div class="container-fluid">
         {Title}
         {Buttons}
         </div>
       </nav>
-  );
+    );
+  }
 }
 
 export default Navbar;
