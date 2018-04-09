18c5976b9a8255bce67608834f02a8e1722a8c5b
Chris <chris.fetterolf@gmail.com>
2018-04-09 17:12:46 -0400

    update login

--- client/src/components/Discover.js
+++ client/src/components/Discover.js
@@ -1,8 +1,16 @@
 import React from 'react';
 //import styled from 'styled-components';
-//import '../css/Discover.css';
+import '../css/Discover.css';
+import {colors} from '../config/constants';
+
+const styles = {
+  form: {
+    backgroundColor: colors.fill,
+  },
+}
 
 class Discover extends React.Component  {
+
   constructor(props) {
     super(props);
 
@@ -17,8 +25,36 @@ class Discover extends React.Component  {
 
   render() {
     return (
-      <div className="container-fluid">
-        Discover here!
+      <div className="container-fluid wrapper">
+        <div className="form" style={styles.form}>
+        <form class="form-style-7">
+          <ul>
+            <li>
+              <label for="name">Name</label>
+              <input type="text" name="name" maxlength="100"/>
+              <span>Enter your full name here</span>
+            </li>
+            <li>
+              <label for="email">Email</label>
+              <input type="email" name="email" maxlength="100"/>
+              <span>Enter a valid email address</span>
+            </li>
+            <li>
+              <label for="url">Website</label>
+              <input type="url" name="url" maxlength="100"/>
+              <span>Your website address (eg: http://www.google.com)</span>
+            </li>
+            <li>
+              <label for="bio">About You</label>
+              <textarea className="email"></textarea>
+              <span>Say something about yourself</span>
+            </li>
+            <li>
+              <input type="submit" value="Send This" />
+            </li>
+          </ul>
+          </form>
+        </div>
       </div>
     );
   }
