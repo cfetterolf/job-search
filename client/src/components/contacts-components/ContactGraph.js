4784b96788d9f5e804ded9525f5ce35e347cd165
Chris <chris.fetterolf@gmail.com>
2018-04-08 22:01:28 -0400

    sigma having trouble with production build

--- client/src/components/contacts-components/ContactGraph.js
+++ client/src/components/contacts-components/ContactGraph.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Sigma, RandomizeNodePositions, RelativeSize} from 'react-sigma';
 //import styled from 'styled-components';
 //import '../css/Contacts.css';
 
@@ -12,65 +11,12 @@ class ContactGraph extends React.Component  {
 
   render() {
 
-    const settings = {
-      drawEdges: true,
-      clone: false,
-      defaultNodeColor: '#C93A3C',
-      defaultEdgeColor: '#F4D8D8',
-    }
-
     return (
       <div>
         Graph
-        <Sigma graph={defaultGraph} settings={settings}>
-
-          <RandomizeNodePositions />
-        </Sigma>
       </div>
     ); // can use <RandomizeNodePositions/> to make nodes random
   }
 }
 
 export default ContactGraph;
-
-const defaultGraph = {
-  "nodes": [
-    {
-      "id": "root",
-      "label": "Your Name Here",
-      "size": 40
-    },
-    {
-      "id": "microsoft",
-      "label": "Microsoft",
-      "size": 15
-    },
-    {
-      "id": "amazon",
-      "label": "Amazon",
-      "size": 40
-    },
-    {
-      "id": "google",
-      "label": "Google",
-      "size": 30
-    },
-  ],
-  "edges": [
-    {
-      "id": "e0",
-      "source": "root",
-      "target": "microsoft"
-    },
-    {
-      "id": "e1",
-      "source": "root",
-      "target": "google"
-    },
-    {
-      "id": "e2",
-      "source": "root",
-      "target": "amazon"
-    },
-  ]
-}
